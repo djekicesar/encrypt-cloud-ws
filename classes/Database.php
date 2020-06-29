f675ed2cfef053afcf28baf6ff74fd672ff5c169
djekicesar <djekicesar@gmail.com>
2020-06-29 16:00:56 +0100

    config PostgresQL 1

--- classes/Database.php
+++ classes/Database.php
@@ -11,14 +11,22 @@ class Database{
         
         try{
             // $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
-            $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;sslmode=require", 
-            $db_host, 
-            $db_port, 
-            $db_name, 
-            $db_username, 
-            $db_password);
+            // $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;sslmode=require", 
+            // $db_host, 
+            // $db_port, 
+            // $db_name, 
+            // $db_username, 
+            // $db_password);
 
-            $conn = new \PDO($conStr);
+            // $conn = new \PDO($conStr);
+            $conn = (function(){
+                $parts = (parse_url(getenv('postgres://jsdydixgxasdpu:8bcfd0a836c26ac3de7cb1a321bbe8addd5058bcf414f56b46280ee02b8d75ba@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d1cou21holpakh')
+                 ?: 'postgresql://jsdydixgxasdpu:8bcfd0a836c26ac3de7cb1a321bbe8addd5058bcf414f56b46280ee02b8d75ba@ec2-34-224-229-81.compute-1.amazonaws.com
+                 :5432/d1cou21holpakh'));
+                extract($parts);
+                $path = ltrim($path, "/");
+                return new PDO("pgsql:host={$host};port={$port};dbname={$path}", $user, $pass);
+            })();
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
