3521f06951d9923eb82406ce1605293cb34c5391
djekicesar <djekicesar@gmail.com>
2020-06-29 11:39:35 +0100

    Database connection

--- classes/Database.php
+++ classes/Database.php
@@ -1,15 +1,15 @@
 <?php
 class Database{
     
-    private $db_host = 'localhost';
-    private $db_name = 'encrypt';
-    private $db_username = 'root';
-    private $db_password = '';
+    private $db_host = 'ec2-34-224-229-81.compute-1.amazonaws.com';
+    private $db_name = 'd1cou21holpakh';
+    private $db_username = 'jsdydixgxasdpu';
+    private $db_password = '8bcfd0a836c26ac3de7cb1a321bbe8addd5058bcf414f56b46280ee02b8d75ba';
     
     public function dbConnection(){
         
         try{
-            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
+            $conn = new PDO("pgsql:dbname=$db_name;host=$db_host", $db_username, $db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
