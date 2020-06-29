b937f565de6c0e53a51b923f9ddbde199741b97d
djekicesar <djekicesar@gmail.com>
2020-06-29 11:58:45 +0100

    problem fix

--- classes/Database.php
+++ classes/Database.php
@@ -5,11 +5,11 @@ class Database{
     private $db_name = 'd1cou21holpakh';
     private $db_username = 'jsdydixgxasdpu';
     private $db_password = '8bcfd0a836c26ac3de7cb1a321bbe8addd5058bcf414f56b46280ee02b8d75ba';
-    
+    private $db_port = 5432;
     public function dbConnection(){
         
         try{
-            $conn = new PDO("pgsql:dbname=$db_name;host=$db_host", $db_username, $db_password);
+            $conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name,$db_username,$db_password");
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
